8e9f180fa2d2a27c0a731e95486a3a61de03dde7
ACrazyTown <47027981+ACrazyTown@users.noreply.github.com>
2025-08-10 22:54:08 +0200

    actual final fix plesae

--- source/props/CutsceneTrigger.hx
+++ source/props/CutsceneTrigger.hx
@@ -7,23 +7,25 @@ import data.LdtkProject.LdtkProject_Entity;
 
 class CutsceneTrigger extends LdtkEntity
 {
+    var called:Bool = false;
+
 	public function new(entity:LdtkProject_Entity, x:Float = 0, y:Float = 0) 
     {
         super(entity, x, y);
-
-        width = 850;
-        height = 450;
+        makeGraphic(850, 450);
+        visible = false;
     }
 
     override function onOverlap(object:FlxObject) 
     {
         super.onOverlap(object);
 
-        if (object is Player)
+        if (object is Player && !called)
         {
 			PlayState.instance.controlsText.visible = false;
 			PlayState.instance.textboxIsOpen = true;
 			PlayState.instance.openSubState(new EndingCutscene());
+            called = true;
         }
     }
 }
